a851f5ac08e075a2948d81c8afb909437dfda2f3
xianleigirl <1911276652@qq.com>
2022-11-23 14:39:10 +0800

    feat: get blockreward of per epoch for all miners between epochs

---
+++ pool-monitor/miners_blockreward.js
@@ -0,0 +1,42 @@
+[
+  {
+    $match: {
+      Epoch: {
+        $gte: ctx.StartEpoch,
+        $lt: ctx.EndEpoch,
+      },
+      Depth: 2,
+      "Msg.From": "02",
+      "Msg.Method": 14,
+    },
+  },
+  {
+    $lookup: {
+      from: "Message",
+      localField: "Cid",
+      foreignField: "_id",
+      as: "blockrewardMatches",
+    },
+  },
+  {
+    $unwind: "$blockrewardMatches",
+  },
+  {
+    $group: {
+      _id: {epoch: "$Epoch", miner: "$Msg.To"},
+      totalBlockReward: {
+        $sum: {
+          $divide: [
+            {
+              $toDecimal: "$blockrewardMatches.Value",
+            },
+            1,
+          ],
+        },
+      },
+      blockcount: {
+        $sum: 1,
+      },
+    },
+  },
+]
