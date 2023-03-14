8e0669faca2d2a931008ceb5edb083c7ac1522c5
xianleigirl <1911276652@qq.com>
2023-03-14 15:55:39 +0800

    fix: all_method

--- pool-monitor/all_methods.js
+++ pool-monitor/all_methods.js
@@ -4,8 +4,9 @@
         $match: {
             $expr:
                 {$and: [
-                    {$eq: ["$Detail.PackedHeight", ctx.StartEpoch]},
-                    // {$lt: ["$Detail.PackedHeight", ctx.EndEpoch]},
+                        {$gt: ["$Nonce", 0]},
+                    {$gte: ["$Detail.PackedHeight", ctx.StartEpoch]},
+                    {$lt: ["$Detail.PackedHeight", ctx.EndEpoch]},
                     {$or: [
                             {$eq: ["1", {$substrBytes: ["$From", 0, 1] }]},
                             {$eq: ["3", {$substrBytes: ["$From", 0, 1] }]},
@@ -22,3 +23,53 @@
         }
     }
 ]
+
+    // todo: epoch range还是全表扫描？
+// ExecTrace
+[
+    {
+        $match: {
+            $expr: {
+                $and: [
+                    {$eq: ["$Depth", 1]},
+                    {$gte: ["$Epoch", ctx.StartEpoch]},
+                    {$lt: ["$Epoch", ctx.EndEpoch]},
+                    {$or: [
+                            {$eq: ["1", {$substrBytes: ["$Msg.From", 0, 1] }]},
+                            {$eq: ["3", {$substrBytes: ["$Msg.From", 0, 1] }]},
+                            {$eq: ["4", {$substrBytes: ["$Msg.From", 0, 1] }]}
+                        ]
+                    }
+                ]
+            }
+        }
+    },
+    {
+        $lookup:
+            {
+                from: "Message",
+                let: {cid: "$Cid"},
+                pipeline: [
+                    {
+                        $match:
+                            {
+                                $expr: {
+                                    $and: [{$eq: [ "$_id", "$$cid"]}]
+                                }
+                            }
+                    }
+                ],
+                as: "blockmessage"
+            }
+    },
+    {
+        $unwind: "$blockmessage"
+    },
+    {
+        $group: {
+            _id: 0,
+            all_methods: {$addToSet: "$Detail.Method"}
+        }
+    }
+]
+
--- pool-monitor/block.js
+++ pool-monitor/block.js
@@ -87,3 +87,5 @@
     }
 ]
 // , {hint:{"Epoch":1,"Depth":1,"Msg.From":1}}
+
+// todo: cold dbs skip过多慢；热库epoch sort慢，感觉是索引没用好
