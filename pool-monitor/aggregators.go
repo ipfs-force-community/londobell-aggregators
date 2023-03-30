ef17acbfb9ece289f8ce311f363abba525fbd546
xianleigirl <1911276652@qq.com>
2023-03-30 15:52:36 +0800

    add batch_trace_for_message.js

--- pool-monitor/aggregators.go
+++ pool-monitor/aggregators.go
@@ -139,6 +139,9 @@ var blockHeaderAggregator []byte
 //go:embed trace_for_message.js
 var traceForMessageAggregator []byte
 
+//go:embed batch_trace_for_message.js
+var batchTraceForMessageAggregator []byte
+
 //go:embed child_transfers_for_message.js
 var childTransfersForMessage []byte
 
@@ -375,6 +378,10 @@ func GetBlockHeaderAggregator() []byte {
 
 func GetTraceForMessageAggregator() []byte {
 	return traceForMessageAggregator
+}
+
+func GetBatchTraceForMessageAggregator() []byte {
+	return batchTraceForMessageAggregator
 }
 
 func GetChildTransfersForMessage() []byte {
---
+++ pool-monitor/batch_trace_for_message.js
@@ -0,0 +1,59 @@
+// ExecTrace
+// Sort by method name
+// todo: 分库查询
+// todo: db.ExecTrace.createIndex({"Epoch":1,"SubCallCount":1}, {"sparse": true});
+[
+    {
+        $match: {
+            $expr: {
+                $and: [
+                    {$eq: ["$Epoch", ctx.StartEpoch]},
+                    {$or: [
+                            {$in: ["$Cid", ctx.Cids]},
+                            {$in: ["$SignedCid", ctx.Cids]}
+                        ]},
+                    {$eq: ["$Depth", 1]} // not inclued cron, which may contained burn pledge
+                ]
+            }
+        }
+    },
+    {
+        $lookup: {
+            from: "Message",
+            localField: "Cid",
+            foreignField: "_id",
+            as: "message",
+        },
+    },
+    {
+        $unwind: "$message"
+    },
+    {
+        $project: {
+            Cid: {
+                $cond: {
+                    if:{
+                        $eq:["$message.SignedCid", null]
+                    }, then: "$message._id",
+                    else: "$message.SignedCid"
+                }
+            },
+            Epoch: "$Epoch",
+            Value: "$message.Value",
+            From: "$message.From",
+            To: "$message.To",
+            ExitCode: "$MsgRct.ExitCode",
+            Method: "$message.Detail.Method",
+            Params: "$message.Params", // []byte
+            Return: "$MsgRct.Return",
+            ParamsDetail: "$message.Detail.Params",
+            ReturnDetail: "$Detail.Return",
+            Version: "$message.Version",
+            Nonce: "$message.Nonce",
+            GasLimit: "$message.GasLimit",
+            GasFeeCap: "$message.GasFeeCap",
+            GasPremium: "$message.GasPremium",
+            GasCost: "$GasCost"
+        }
+    }
+]
