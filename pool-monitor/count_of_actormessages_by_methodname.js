4a6cdf4e7c247f09723ec546827497104d30b8b8
xianleigirl <1911276652@qq.com>
2023-04-04 11:14:29 +0800

    fix: opt

--- pool-monitor/count_of_actormessages_by_methodname.js
+++ pool-monitor/count_of_actormessages_by_methodname.js
@@ -1,20 +1,81 @@
-// Message
+// ExecTrace
 [
     {
         $match: {
             $expr: {
                 $and: [
-                    {$gte: ["$Detail.PackedHeight", ctx.StartEpoch]},
-                    {$lt: ["$Detail.PackedHeight", ctx.EndEpoch]}
+                    {$eq: ["$Depth", 1]},
+                    {$gte: ["$Epoch", ctx.StartEpoch]},
+                    {$lt: ["$Epoch", ctx.EndEpoch]},
+                    {$or: [
+                            {$eq: ["1", {$substrBytes: ["$Msg.From", 0, 1] }]},
+                            {$eq: ["3", {$substrBytes: ["$Msg.From", 0, 1] }]},
+                            {$eq: ["4", {$substrBytes: ["$Msg.From", 0, 1] }]}
+                        ]
+                    },
                 ]
             }
         }
     },
     {
+        $project: {
+            _id: 0,
+            Cid: "$Cid"
+        }
+    },
+    {
+        $lookup: {
+            from: "Message",
+            localField: "Cid",
+            foreignField: "_id",
+            as: "message",
+        }
+    },
+    {
+        $unwind: "$message"
+    },
+    {
+        $project: {
+            _id: 0,
+            Method: "$message.Detail.Method",
+            From: "$message.From",
+            To: "$message.To"
+        }
+    },
+    {
         $group: {
-            _id: "$Detail.Method",
+            _id: "$Method",
             all_froms: {$push: "$From"},
             all_tos: {$push: "$To"},
         }
     }
 ]
+
+// // Message
+// [
+//     {
+//         $match: {
+//             $expr: {
+//                 $and: [
+//                     {$gte: ["$Detail.PackedHeight", ctx.StartEpoch]},
+//                     {$lt: ["$Detail.PackedHeight", ctx.EndEpoch]}
+//                 ]
+//             }
+//         }
+//     },
+//     {
+//         $project: {
+//             _id: 0,
+//             Method: "$Detail.Method",
+//             From: "$From",
+//             To: "$To"
+//         }
+//     },
+//     {
+//         $group: {
+//             _id: "$Method",
+//             all_froms: {$push: "$From"},
+//             all_tos: {$push: "$To"},
+//         }
+//     }
+// ]
--- pool-monitor/count_of_blockmessages_by_methodname.js
+++ pool-monitor/count_of_blockmessages_by_methodname.js
@@ -1,19 +1,69 @@
-// Message
+// ExecTrace
 [
     {
         $match: {
             $expr: {
                 $and: [
-                    {$gte: ["$Detail.PackedHeight", ctx.StartEpoch]},
-                    {$lt: ["$Detail.PackedHeight", ctx.EndEpoch]}
+                    {$eq: ["$Depth", 1]},
+                    {$gte: ["$Epoch", ctx.StartEpoch]},
+                    {$lt: ["$Epoch", ctx.EndEpoch]},
+                    {$or: [
+                            {$eq: ["1", {$substrBytes: ["$Msg.From", 0, 1] }]},
+                            {$eq: ["3", {$substrBytes: ["$Msg.From", 0, 1] }]},
+                            {$eq: ["4", {$substrBytes: ["$Msg.From", 0, 1] }]}
+                        ]
+                    },
                 ]
             }
         }
     },
     {
+        $project: {
+            _id: 0,
+            Cid: "$Cid"
+        }
+    },
+    {
+        $lookup: {
+            from: "Message",
+            localField: "Cid",
+            foreignField: "_id",
+            as: "message",
+        }
+    },
+    {
+        $unwind: "$message"
+    },
+    {
+        $project: {
+            _id: 0,
+            method: "$message.Detail.Method"
+        }
+    },
+    {
         $group: {
-            _id: "$Detail.Method",
+            _id: "$method",
             count: {$sum: 1}
         }
     }
 ]
+
+////Message
+// [
+//     {
+//         $match: {
+//             $expr: {
+//                 $and: [
+//                     {$gte: ["$Detail.PackedHeight", ctx.StartEpoch]},
+//                     {$lt: ["$Detail.PackedHeight", ctx.EndEpoch]}
+//                 ]
+//             }
+//         }
+//     },
+//     {
+//         $group: {
+//             _id: "$Detail.Method",
+//             count: {$sum: 1}
+//         }
+//     }
+// ]
