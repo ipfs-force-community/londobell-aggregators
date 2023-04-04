fffdc0187708b2a667315276fec7d1299ea7fb91
xianleigirl <1911276652@qq.com>
2023-04-04 11:27:49 +0800

    fix: opt

--- pool-monitor/count_of_actormessages_by_methodname.js
+++ pool-monitor/count_of_actormessages_by_methodname.js
@@ -1,12 +1,13 @@
 // ExecTrace
+// db.ExecTrace.createIndex({"Epoch":1,"Depth":1,"Msg.From":1}, {"sparse": true});
 [
     {
         $match: {
             $expr: {
                 $and: [
-                    {$eq: ["$Depth", 1]},
                     {$gte: ["$Epoch", ctx.StartEpoch]},
                     {$lt: ["$Epoch", ctx.EndEpoch]},
+                    {$eq: ["$Depth", 1]},
                     {$or: [
                             {$eq: ["1", {$substrBytes: ["$Msg.From", 0, 1] }]},
                             {$eq: ["3", {$substrBytes: ["$Msg.From", 0, 1] }]},
