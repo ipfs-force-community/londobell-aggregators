12553a29a7a3c56211a6cc3a016e52d1fb0a6ff1
xianleigirl <1911276652@qq.com>
2023-04-11 22:43:07 +0800

    fix

--- pool-monitor/actormessages_by_methodname.js
+++ pool-monitor/actormessages_by_methodname.js
@@ -15,8 +15,8 @@
                         ]
                     },
                     {$or:[
-                            {$eq: ["$Msg.From", ctx.Addr]},
-                            {$eq: ["$Msg.To", ctx.Addr]}
+                            {$in: ["$Msg.From", ctx.Addrs]},
+                            {$in: ["$Msg.To", ctx.Addrs]}
                         ]
                     },
                 ]
