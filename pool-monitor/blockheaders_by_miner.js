70b125766d15f9a0cfa7f1ceba7dbdfc5e628e92
xianleigirl <1911276652@qq.com>
2023-05-12 09:24:52 +0800

    fix:blockheader_by_miner

--- pool-monitor/blockheaders_by_miner.js
+++ pool-monitor/blockheaders_by_miner.js
@@ -3,7 +3,7 @@
     {
         $match: {
             "Epoch": {$gte: ctx.StartEpoch, $lt: ctx.EndEpoch},
-            Miners: {$in: ctx.Addrs}
+            Miner: {$in: ctx.Addrs}
 
             // $expr: {
             //     $and: [
