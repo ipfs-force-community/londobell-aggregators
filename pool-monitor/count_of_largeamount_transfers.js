de67946df6834ae288f2538e49cef6431d2d73b6
xianleigirl <1911276652@qq.com>
2023-05-23 16:18:45 +0800

    fix: opt

--- pool-monitor/count_of_largeamount_transfers.js
+++ pool-monitor/count_of_largeamount_transfers.js
@@ -7,12 +7,12 @@
             "Msg.Value": {$regex: "^.{23,}$"} // todo: 2e22
         }
     },
-    {
-        $project: {
-            _id: 0,
-            Cid: 1
-        }
-    },
+    // {
+    //     $project: {
+    //         _id: 0,
+    //         Cid: 1
+    //     }
+    // },
     {
         $group: {
             _id: 0,
