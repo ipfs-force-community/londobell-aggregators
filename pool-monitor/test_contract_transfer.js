ae2a42c08f60fcdde8ce88d6b5841598b34e873a
xianleigirl <1911276652@qq.com>
2023-07-06 11:35:19 +0800

    test: contract_transfer

---
+++ pool-monitor/test_contract_transfer.js
@@ -0,0 +1,63 @@
+// ExecTrace
+[
+    {
+        $match: {
+            IsBlock: true,
+            Epoch: {$gte: ctx.StartEpoch, $lt: ctx.EndEpoch},
+            "Msg.MethodName": "InvokeContract"
+        }
+    },
+    {
+        $lookup: {
+            from: "Message",
+            let: {cid: "$Cid"},
+            pipeline: [
+                {
+                    $match:
+                        {
+                            $expr: {
+                                $and: [
+                                    {$eq: ["$_id", "$$cid"]}
+                                ]
+                            }
+                        }
+                }
+            ],
+            as: "message"
+        }
+    },
+    {
+        $unwind: "$message"
+    },
+    {
+        $project: {
+            ID: "$_id",
+            Cid: "$Cid",
+            SignedCid: "$SignedCid",
+            Epoch: "$Epoch",
+            Seq: "$Seq",
+            Depth: "$Depth",
+            Ver: "$Ver",
+            Msg: "$Msg",
+            MsgRct: "$MsgRct",
+            Error: "$Error",
+            SeqIndex: "$SeqIndex",
+            SubCallCount: "$SubCallCount",
+            GasCost: "$GasCost",
+            ReturnBson: "$MsgRct.Return",
+            Version: "$message.Version",
+            To: "$message.To",
+            From: "$message.From",
+            Nonce: "$message.Nonce",
+            Value: "$message.Value",
+            GasLimit: "$message.GasLimit",
+            GasFeeCap: "$message.GasFeeCap",
+            GasPremium: "$message.GasPremium",
+            Method: "$message.Method",
+            ParamsBson: "$message.Params",
+            Detail: "$message.Detail",
+            Actor: "$message.Detail.Actor",
+            IsBlock: "$IsBlock"
+        }
+    }
+]
