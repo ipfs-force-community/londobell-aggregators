43c644187491d427a113ac7cec674a75ceba02c7
xianleigirl <1911276652@qq.com>
2023-08-04 15:51:07 +0800

    feat: users for explicitmessage & actors by type

--- pool-monitor/gascosts_for_actor.js
+++ pool-monitor/gascosts_for_actor.js
@@ -4,7 +4,6 @@
         $match: {
             ActorID: ctx.Addr,
             IsBlock: true,
-            Type: "from",
             Epoch: {$gte: ctx.StartEpoch, $lt: ctx.EndEpoch}
         }
     },
