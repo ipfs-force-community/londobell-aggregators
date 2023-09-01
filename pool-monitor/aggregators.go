35c3a902f4fb5e7017543c8edaf4584faa02609f
xianleigirl <1911276652@qq.com>
2023-09-01 10:20:14 +0800

    fix: bisearch transfer

--- pool-monitor/aggregators.go
+++ pool-monitor/aggregators.go
@@ -337,11 +337,20 @@ var tipsetsListAggregator []byte
 //go:embed actormessages_by_methodname_noskip.js
 var actorMessagesByMethodNameNoskipAggregator []byte
 
+//go:embed transfermsgs_for_actor_noskip.js
+var transferMsgsFroActorNoSkipAggregator []byte
+
+//go:embed transfer_type_for_actor_noskip.js
+var transferTypeForActorNoSkipAggregator []byte
+
+//go:embed transfer_send_and_receive_for_actor_noskip.js
+var transferSendAndReceiveForActorNoSkipAggregator []byte
+
 func GetAddressAggregator() []byte {
 	return addressAggregator
 }
 
-func GetActorMessagesByMethodNameNoskipAggregator()[]byte  {
+func GetActorMessagesByMethodNameNoskipAggregator() []byte {
 	return actorMessagesByMethodNameNoskipAggregator
 }
 
@@ -759,4 +768,16 @@ func GetCountOfTipsetAggregator() []byte {
 
 func GetTipsetsListAggregator() []byte {
 	return tipsetsListAggregator
+}
+
+func GetTransferMsgsFroActorNoSkipAggregator() []byte {
+	return transferMsgsFroActorNoSkipAggregator
+}
+
+func GetTransferTypeForActorNoSkipAggregator() []byte {
+	return transferTypeForActorNoSkipAggregator
+}
+
+func GetTransferSendAndReceiveForActorNoSkipAggregator() []byte {
+	return transferSendAndReceiveForActorNoSkipAggregator
 }
